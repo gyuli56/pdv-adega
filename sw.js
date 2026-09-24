a3d33749782ab61e95d081c1c6ae436ca9181077
adega <facundogyuliana56@gmail.com>
2026-09-24 19:11:28 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,11 @@
-const C='pdv-v4';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='pdv-v4-1';
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(C).then(cache=>cache.addAll(['./','./index.html','./manifest.webmanifest'])));
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',event=>{
+  event.respondWith(caches.match(event.request).then(r=>r||fetch(event.request)));
+});
